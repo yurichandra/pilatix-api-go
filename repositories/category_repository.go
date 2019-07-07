f14eb4a32cfd77ccf818d49e5d2402c3ea4197cf
Yuri Chandra <yurichandra@gmail.com>
2019-07-07 23:43:49 +0700

    Simplified variables

--- repositories/category_repository.go
+++ repositories/category_repository.go
@@ -12,32 +12,32 @@ type CategoryRepository struct {
 }
 
 // Get return all categories.
-func (repo *CategoryRepository) Get() []models.Category {
+func (r *CategoryRepository) Get() []models.Category {
 	categories := make([]models.Category, 0)
-	repo.db.Find(&categories)
+	r.db.Find(&categories)
 
 	return categories
 }
 
 // Find return single category.
-func (repo *CategoryRepository) Find(id uint) models.Category {
+func (r *CategoryRepository) Find(id uint) models.Category {
 	category := models.Category{}
-	repo.db.First(&category, id)
+	r.db.First(&category, id)
 
 	return category
 }
 
 // Create creates new category model.
-func (repo *CategoryRepository) Create(data *models.Category) error {
-	return repo.db.Create(&data).Error
+func (r *CategoryRepository) Create(data *models.Category) error {
+	return r.db.Create(&data).Error
 }
 
 // Update updates category model.
-func (repo *CategoryRepository) Update(data *models.Category) error {
-	return repo.db.Save(&data).Error
+func (r *CategoryRepository) Update(data *models.Category) error {
+	return r.db.Save(&data).Error
 }
 
 // Delete deletes category by ID.
-func (repo *CategoryRepository) Delete(id uint) error {
-	return repo.db.Where("id = ?", id).Delete(models.Category{}).Error
+func (r *CategoryRepository) Delete(id uint) error {
+	return r.db.Where("id = ?", id).Delete(models.Category{}).Error
 }
