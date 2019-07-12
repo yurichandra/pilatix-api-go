730493804e2e0a2bb74d79cd86e6b1cebb844070
Yuri Chandra <yurichandra@gmail.com>
2019-07-12 20:43:58 +0700

    Added CategoryRepositoryMock

---
+++ mocks/category_repository_mock.go
@@ -0,0 +1,47 @@
+package mocks
+
+import (
+	"pilatix-api-go/models"
+
+	"github.com/stretchr/testify/mock"
+)
+
+// CategoryRepositoryMock represent mocking of CategoryRepository.
+type CategoryRepositoryMock struct {
+	mock.Mock
+}
+
+// Get mock Get method of CategoryRepository.
+func (r *CategoryRepositoryMock) Get() []models.Category {
+	args := r.Called()
+
+	return args.Get(0).([]models.Category)
+}
+
+// Find mock Find method of CategoryRepository.
+func (r *CategoryRepositoryMock) Find(id uint) models.Category {
+	args := r.Called(id)
+
+	return args.Get(0).(models.Category)
+}
+
+// Create mock Create method of CategoryRepository.
+func (r *CategoryRepositoryMock) Create(data *models.Category) error {
+	args := r.Called(data)
+
+	return args.Error(0)
+}
+
+// Update mock Update method of CategoryRepository.
+func (r *CategoryRepositoryMock) Update(data *models.Category) error {
+	args := r.Called(data)
+
+	return args.Error(0)
+}
+
+// Delete mock Delete method of CategoryRepository.
+func (r *CategoryRepositoryMock) Delete(id uint) error {
+	args := r.Called(id)
+
+	return args.Error(0)
+}
